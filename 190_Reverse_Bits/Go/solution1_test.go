9499265cebffd23737a1083ab2cfb6fdf544ef50
igormatevosian <igormatevosian@gmail.com>
2025-02-18 01:32:50 +0300

    feat(190): go solution implemented

---
+++ 190_Reverse_Bits/Go/solution1_test.go
@@ -0,0 +1,21 @@
+package Go
+
+import "testing"
+
+func TestCase1(t *testing.T) {
+	expected := uint32(964176192)
+	actual := ReverseBits(43261596)
+
+	if expected != actual {
+		t.Errorf("expected = %d, actual = %d", expected, actual)
+	}
+}
+
+func TestCase2(t *testing.T) {
+	expected := uint32(3221225471)
+	actual := ReverseBits(4294967293)
+
+	if expected != actual {
+		t.Errorf("expected = %d, actual = %d", expected, actual)
+	}
+}
