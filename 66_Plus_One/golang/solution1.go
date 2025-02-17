d4453e5a6e90550c00cf279f3593956d5c22c7fd
igormatevosian <igormatevosian@gmail.com>
2025-02-18 02:24:06 +0300

    feat(66): go solution implemented

---
+++ 66_Plus_One/golang/solution1.go
@@ -0,0 +1,15 @@
+package golang
+
+func PlusOne(digits []int) []int {
+	for i := len(digits) - 1; i >= 0; i-- {
+		if digits[i] == 9 {
+			digits[i] = 0
+		} else {
+			digits[i] += 1
+			return digits
+		}
+	}
+	res := make([]int, len(digits)+1)
+	res[0] = 1
+	return res
+}
---
+++ 66_Plus_One/golang/solution1_test.go
@@ -0,0 +1,39 @@
+package golang
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCase1(t *testing.T) {
+	input := []int{1, 2, 3}
+	expected := []int{1, 2, 4}
+
+	actual := PlusOne(input)
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("expected:%v, actual:%v", expected, actual)
+	}
+}
+
+func TestCase2(t *testing.T) {
+	input := []int{4, 3, 2, 1}
+	expected := []int{4, 3, 2, 2}
+
+	actual := PlusOne(input)
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("expected:%v, actual:%v", expected, actual)
+	}
+}
+
+func TestCase3(t *testing.T) {
+	input := []int{9}
+	expected := []int{1, 0}
+
+	actual := PlusOne(input)
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("expected:%v, actual:%v", expected, actual)
+	}
+}
