ec9044cc92daef8875f92301b7ca90439b8770cd
igormatevosian <igormatevosian@gmail.com>
2025-02-26 01:33:31 +0300

    feat(283): go solution implemented

---
+++ 283-move-zeroes/golang/solution.go
@@ -0,0 +1,15 @@
+package golang
+
+func moveZeroes(nums []int) {
+	index := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != 0 {
+			nums[index] = nums[i]
+			index++
+		}
+	}
+	for index < len(nums) {
+		nums[index] = 0
+		index++
+	}
+}
