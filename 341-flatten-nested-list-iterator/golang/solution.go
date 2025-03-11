24a6c023153d10700bf1c728493d52e3cfbd7a17
igormatevosian <igormatevosian@gmail.com>
2025-03-12 01:05:15 +0300

    feat(341): go solution implemented

---
+++ 341-flatten-nested-list-iterator/golang/solution.go
@@ -0,0 +1,58 @@
+package golang
+
+import "slices"
+
+// This is the interface that allows for creating nested lists.
+// You should not implement it, or speculate about its implementation
+type NestedInteger struct {
+}
+
+// Return true if this NestedInteger holds a single integer, rather than a nested list.
+func (this NestedInteger) IsInteger() bool {}
+
+// Return the single integer that this NestedInteger holds, if it holds a single integer
+// The result is undefined if this NestedInteger holds a nested list
+// So before calling this method, you should have a check
+func (this NestedInteger) GetInteger() int {}
+
+// Set this NestedInteger to hold a single integer.
+func (n *NestedInteger) SetInteger(value int) {}
+
+// Set this NestedInteger to hold a nested list and adds a nested integer to it.
+func (this *NestedInteger) Add(elem NestedInteger) {}
+
+// Return the nested list that this NestedInteger holds, if it holds a nested list
+// The list length is zero if this NestedInteger holds a single integer
+// You can access NestedInteger's List element directly if you want to modify it
+func (this NestedInteger) GetList() []*NestedInteger {}
+
+type NestedIterator struct {
+	stack []*NestedInteger
+}
+
+func Constructor(nestedList []*NestedInteger) *NestedIterator {
+	reversed := make([]*NestedInteger, len(nestedList))
+	copy(reversed, nestedList)
+	slices.Reverse(reversed)
+	return &NestedIterator{stack: reversed}
+}
+
+func (this *NestedIterator) Next() int {
+	val := this.stack[len(this.stack)-1].GetInteger()
+	this.stack = this.stack[:len(this.stack)-1]
+	return val
+}
+
+func (this *NestedIterator) HasNext() bool {
+	for len(this.stack) > 0 {
+		if this.stack[len(this.stack)-1].IsInteger() {
+			return true
+		}
+		list := this.stack[len(this.stack)-1].GetList()
+		this.stack = this.stack[:len(this.stack)-1]
+		for i := len(list) - 1; i >= 0; i-- {
+			this.stack = append(this.stack, list[i])
+		}
+	}
+	return false
+}
