e615aac64fd3e5701304836796955fd38b3a3e13
igormatevosian <igormatevosian@gmail.com>
2025-03-20 22:38:54 +0300

    feat(104): python solution implemented

---
+++ 104-maximum-depth-of-binary-tree/python/solution.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        res = 0
+        stack = [root]
+        while stack:
+            res += 1
+            new_stack = []
+            for node in stack:
+                if node.left:
+                    new_stack.append(node.left)
+                if node.right:
+                    new_stack.append(node.right)
+            stack = new_stack
+
+        return res
